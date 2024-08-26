a92a3f9b0360e92b2be6c6d7ec1a79bb5812f5e8
Denelle97 <115361585+Denelle97@users.noreply.github.com>
2024-08-26 16:58:53 +0200

    Add files via upload

---
+++ air-bnb-clone/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
+
+export default function App() {
+  
+  return (
+    <div>
+         <Navbar />
+         <Hero />
+         <Card
+             img="katie-zaferes.png"
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+                />
+    </div>
+  )
+}
